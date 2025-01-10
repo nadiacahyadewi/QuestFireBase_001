95cb6154979cdeb75471dfdf8edfa404f614b860
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-01-10 22:35:26 +0700

    menambahkan data fungsi MahasiswaEvent pada InsertViewModel.kt

--- app/src/main/java/com/example/firebase/ui/home/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/firebase/ui/home/viewmodel/InsertViewModel.kt
@@ -30,3 +30,14 @@ data class MahasiswaEvent(
     val kelas: String = "",
     val angkatan:String= ""
 )
+
+// menyimpan input form kedalam entity
+fun MahasiswaEvent.toMhsModel(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    jenisKelamin = jenisKelamin,
+    alamat = alamat,
+    kelas = kelas,
+    angkatan = angkatan
+
+)

6233c8cbd99e43140ab60f3dfbfbdd7ede59e29b
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-01-15 15:53:09 +0700

    menambah data judulSkripsi dan dosenPembimbing di dataclass Mahasiswa

--- app/src/main/java/com/example/firebase/model/Mahasiswa.kt
+++ app/src/main/java/com/example/firebase/model/Mahasiswa.kt
@@ -6,7 +6,9 @@ data class Mahasiswa (
     val alamat: String,
     val jenisKelamin: String,
     val kelas: String,
-    val angkatan: String
+    val angkatan: String,
+    val judulSkripsi: String,
+    val dosenPembimbing: String
 ){
-    constructor(): this("", "", "", "", "", "")
+    constructor(): this("", "", "", "", "", "", "", "" )
 }
